4101d0cebd6d1f49fad7d08b67e9454633cb0455
RamaDev17 <ramdhaney.dev17@gmail.com>
2024-05-30 14:18:50 +0700

    make model

--- app/Models/CourseAnswer.php
+++ app/Models/CourseAnswer.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CourseAnswer extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [
+        'id'
+    ];
+
+    public function question(){
+        return $this->belongsTo(CourseQuestion::class, 'question_id', 'id');
+    }
 }
